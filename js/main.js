04acefbcf9799e67557a5d8ef914db90be950cab
Caribla <caribla4@gmail.com>
2024-02-12 20:49:39 +0100

    Fixing number generation and interaction with no mine cells : Number and change color onclick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const FlippingCells = (cell) => {
+    const cellClicked = document.getElementById(cell);
+    for(let i = 0; i < cellClicked.classList.length; i++){
+        if(cellClicked.classList[i].match("lightGreenCell")){
+            cellClicked.classList.remove("lightGreenCell")
+            cellClicked.classList.add("clickedCellLightGreen");
+        } else if (cellClicked.classList[i] == "greenCell"){
+            cellClicked.classList.remove("greenCell")
+            cellClicked.classList.add("clickedCellGreen");
+        }
+        
+    }
+    
+} 
+
 // ==============================================================
 // GENERATION OF MINESWEEPER FIELD INSIDE THE CONTAINER
 // ==============================================================
@@ -13,7 +28,8 @@ for (let i = 0; i < 20; i++) {
 
     for (let j = 0; j < 24; j++) { 
         // Create a cell 
-        let cell = document.createElement('fieldCell'); 
+        let cell = document.createElement('div');
+        cell.setAttribute('id', 'cell_'+(([i,j]).toString()));
 
         if ((i + j) % 2 == 0) { 
             cell.setAttribute('class', 'cell lightGreenCell'); 
@@ -51,9 +67,29 @@ while(minesToPlace > 0){
 for(let i = 0; i < cells.length; i++){
     if(!cells[i].classList.contains("mine")){
         let minesAround = 0;
-        let cellsAround = [i-25, i-24, i-23, i-1, i+1, i+23, i+24, i+25];
+        let cellsAround;
+
+        if(i == 0){
+            cellsAround = [i+1, i+24, i+25];
+        } else if (i == 23){
+            cellsAround = [i-1, i+23, i+24];
+        } else if (i == 456){
+            cellsAround = [i-24, i-23, i+1];
+        } else if (i == 479){
+            cellsAround = [i-25, i-24, i-1];
+        } else if (i > 0 && i < 23){
+            cellsAround = [i-1, i+1, i+23, i+24, i+25];
+        } else if (i > 456 && i < 479){
+            cellsAround = [i-25, i-24, i-23, i-1, i+1];
+        } else if (i > 0 && i % 24 == 0 && i < 456){
+            cellsAround = [i-24, i-23, i+1, i+24, i+25];
+        } else if (i > 23 && i % 24 == 23 && i < 479){
+            cellsAround = [i-25, i-24, i-1, i+23, i+24];
+        } else {
+            cellsAround = [i-25, i-24, i-23, i-1, i+1, i+23, i+24, i+25];
+        }
         for(let j=0; j<cellsAround.length; j++){
-            if(cells[cellsAround[j]] != undefined && cells[cellsAround[j]].classList.contains("mine")){
+            if(cells[cellsAround[j]].classList.contains("mine")){
                 minesAround++;
             }
         }
@@ -61,3 +97,30 @@ for(let i = 0; i < cells.length; i++){
     }
 }
 
+// ==============================================================
+// FUNCTIONALITIES OF THE MINESWEEPER - SET CLICKERS FOR EACH CELL
+// ==============================================================
+
+const arrayCells = Array.from(cells);
+
+arrayCells.forEach(item => {
+    item.addEventListener("click", (e) => {
+        let classes = Array.from(e.target.classList);
+        let numMines;
+        for(let i=0; i<classes.length; i++){
+            (classes[i].includes("minesAround-")) ? numMines = parseInt(classes[i].slice(-1)) : numMines = "mine";
+        }
+        switch(numMines){
+            case "mine":
+                //TODO: GameOver interaction goes right here
+                break;
+            default:
+                FlippingCells(e.target.id);
+                e.target.classList.add("minesAround"+numMines);
+                if(numMines != 0){
+                    e.target.innerHTML = numMines;
+                }
+                break;
+        }
+    })
+})
